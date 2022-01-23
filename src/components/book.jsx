0a8257872ac4954d091e7ba1d28bcaa0e783bef2
EdenGenetTasama <edentasama19@gmail.com>
2022-01-23 13:35:39 +0200

    add h1 element

---
+++ src/components/book.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+
+export default class  BookCom extends React.Component{
+constructor(props){
+    super(props)
+this.state = {inputValue: " "};
+this.GetWritterName = this.GetWritterName.bind(this);
+this.InputOnChange = this.InputOnChange.bind(this);
+
+}
+
+GetWritterName(){
+    this.setState({inputValue: this.state.inputValue});
+}
+
+InputOnChange(event){
+    this.state.inputValue  = event.target.value;
+}
+
+componentWillMount(){
+    alert("Welcome to the page ☺")
+}
+componentDidMount(){
+    console.log("Component load Up!");
+}
+
+componentWillUpdate(){
+    alert("Before Update Writher Name")
+}
+componentDidUpdate(){
+console.log("UPDATE HAPPENED")
+}
+
+render(){
+    return(
+        <div>
+            <h1>{this.props.nameOfBook}</h1>
+            <input type="text" onChange={this.InputOnChange}/>
+            <button type="button"  onClick={this.GetWritterName} >click to change component</button>
+            <h1>{this.state.inputValue}</h1>
+        </div>
+    )
+}
+
+}
